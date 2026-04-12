eb4985d472d6f60213fa45d69b8d9149f35f500a
Dallasregenerative <220727426+Dallasregenerative@users.noreply.github.com>
2026-04-12 16:30:43 -0400

    Fix N-259 and WL edge function meta descriptions - remove old Melatonin references

--- client/src/pages/BecomeAdvisor.tsx
+++ client/src/pages/BecomeAdvisor.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import { Link } from "wouter";
 import { ArrowLeft, Send, Shield, Users, BookOpen, Microscope } from "lucide-react";
 
